3e062261eb30dc6f5a78ff41c71eb37292a242d2
Neil Gaspar <neil.gaspar@dcmail.ca>
2018-02-05 08:36:24 -0500

    site

--- footer.php
+++
@@ -1,9 +0,0 @@
-</div>	<!-- Closes 'content' from header -->	
-		<div id="footer"> 				
-
-		</div>
-	</div> <!-- Closes 'content-container' from header -->
-</div> <!-- Closes 'container' from header -->
-
-</body>
-</html>
--- index.php
+++
@@ -1,8 +0,0 @@
-<!-- start of main page content. --> 
-
-<h1>Welcome to my Neil Gaspar's Webpages</h1>
-<hr/>
-<p>
-	Name: Neil Gaspar<br/>
-	Course Details: MODE 4201
-</p>
