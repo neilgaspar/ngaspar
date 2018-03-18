eea7aeda12b6bc3aa05e654f93bfcc9618c18ad4
Neil Gaspar <neil.gaspar@dcmail.ca>
2018-03-18 15:14:52 -0400

    information

--- javascript/Login.js
+++ javascript/Login.js
@@ -6,21 +6,36 @@ function Login(){
 
     //Check the username for something entered and if correct
     if(nameText.length > 0)
+    {
         if(nameText != "User123")
-            displayString += "Username does not exist.<br/>"
+        {
+            displayString += "Username does not exist.<br/>";
+        }
+    }
     else
-        displayString += "You must enter something for your username.<br/>"
-
+    {
+        displayString += "You must enter something for your username.<br/>";
+    }
     //Check the password  for something entered and if correct
     if(password.length > 0)
-        if(password != "password")
-            displayString += "password does not exist.<br/>"
+    {
+        if(password != "Information")
+        {
+             displayString += "Password does not exist.<br/>";
+        }
+    }
     else
-        displayString += "You must enter something for your password.<br/>"
+    {
+        displayString += "You must enter something for your password.<br/>";
+    }
 
     //Display error or go to new page
     if(displayString.length == 0)
+    {
         document.location.href = "./information.html";
+    }
     else
+    {
         document.getElementById("errorMessage").innerHTML = displayString;
+    }
 }
