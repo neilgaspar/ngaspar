93f88586781e334b3c9341d96a9542af77c29ed8
Neil Gaspar <neil.gaspar@dcmail.ca>
2018-03-18 14:28:16 -0400

    First commit

---
+++ javascript/Login.js
@@ -0,0 +1,26 @@
+function Login(){
+
+    var nameText = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var displayString = "";
+
+    //Check the username for something entered and if correct
+    if(nameText.length > 0)
+        if(nameText != "User123")
+            displayString += "Username does not exist.<br/>"
+    else
+        displayString += "You must enter something for your username.<br/>"
+
+    //Check the password  for something entered and if correct
+    if(password.length > 0)
+        if(password != "password")
+            displayString += "password does not exist.<br/>"
+    else
+        displayString += "You must enter something for your password.<br/>"
+
+    //Display error or go to new page
+    if(displayString.length == 0)
+        document.location.href = "./information.html";
+    else
+        document.getElementById("errorMessage").innerHTML = displayString;
+}
